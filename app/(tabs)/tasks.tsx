f585718863d173b9641be862165c0d1aca8fc7ad
ZimboSebastiao <gloriasebastiao7@gmail.com>
2025-07-04 08:05:29 -0300

    estilização concluída

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -1,7 +1,8 @@
 import { getCategoryById } from '@/utils/categoryUtils';
 import { deleteTask, getHabitById, getTasks, Habit, saveTask, Task } from '@/utils/storage';
-import { Feather, FontAwesome } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Play, X } from 'lucide-react-native';
 import React, { useEffect, useState } from 'react';
 import {
 	Alert,
@@ -119,7 +120,6 @@ const handleDeleteTask = async (taskId: string) => {
       }
     });
 
-    // Converter para o formato SectionList e calcular tempo total
     const sections: GroupedTasks[] = Object.keys(grouped)
       .filter(key => grouped[key].length > 0)
       .map(key => {
@@ -191,116 +191,122 @@ const handleDeleteTask = async (taskId: string) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tarefas para: {habit?.title}</Text>
+		<View style={styles.titleIconContainer}>
+			<Text style={styles.titleContainer}>Plano Diário <Text style={styles.title}>{habit?.title}</Text></Text>
+			<Pressable 
+			onPress={() => router.push('/')}
+			style={({ pressed }) => [
+				styles.iconView,
+				{
+				transform: [{ scale: pressed ? 0.95 : 1 }],
+				opacity: pressed ? 0.8 : 1,
+				}
+			]}
+			>
+			{({ pressed }) => (
+				<X 
+				size={34} 
+				color={pressed ? '#A278C6' : '#000000'}
+				/>
+			)}
+			</Pressable>
+		</View>
       
-      <SectionList
-        sections={groupedTasks}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Card style={styles.taskCard}>
-
-<Card.Content>
-  <View style={styles.taskHeader}>
-    <Text style={styles.taskTitle}>{item.title}</Text>
-    <View style={styles.taskActions}>
-      {!isNotesCategory && !item.completed && (
-        <Pressable 
-          onPress={() => handleTimerAction(item.id)}
-          style={styles.actionButton}
-        >
-          <FontAwesome name="play" size={20} color="#FF7617" />
-        </Pressable>
-      )}
-      {!isNotesCategory && item.timeSpent && (
-        <Text style={styles.timeText}>{formatTaskTime(item.timeSpent)}</Text>
-      )}
-      <Pressable 
-        onPress={() => handleDeleteTask(item.id)}
-        style={styles.actionButton}
-      >
-        <Feather name="trash-2" size={20} color="#ff3b30" />
-      </Pressable>
-    </View>
-  </View>
-  
-  {isNotesCategory && item.notes && (
-    <Text style={styles.taskNotes}>{item.notes}</Text>
-  )}
-  
-  <Text style={styles.createdAt}>
-    Criado em: {new Date(item.createdAt).toLocaleDateString()}
-  </Text>
-</Card.Content>
-          </Card>
-        )}
-        renderSectionHeader={({ section }) => (
-          <View style={styles.sectionHeader}>
-            <Text style={styles.sectionTitle}>{section.title}</Text>
-            {!isNotesCategory && (
-              <Text style={styles.sectionTime}>Total: {section.totalTime}</Text>
-            )}
-          </View>
-        )}
-        contentContainerStyle={styles.listContent}
-        ListEmptyComponent={
-          <Text style={styles.emptyText}>Nenhuma tarefa criada ainda</Text>
-        }
-      />
       
-     
-      <Pressable
-        style={styles.addButton}
-        onPress={() => setModalVisible(true)}
-      >
-        <Feather name="plus" size={24} color="white" />
-      </Pressable>
+      	<SectionList
+			sections={groupedTasks}
+			keyExtractor={(item) => item.id}
+			renderItem={({ item }) => (
+          	<Card style={styles.taskCard}>
+				<Card.Content>
+					<View style={styles.taskHeader}>
+						<Text style={styles.taskTitle}>{item.title}</Text>
+						<View style={styles.taskActions}>
+						{!isNotesCategory && !item.completed && (
+							<Pressable 
+							onPress={() => handleTimerAction(item.id)}
+							style={styles.actionButton}
+							>
+							<View style={styles.playButton}>
+								<Play  size={20} color="white" />
+							</View>
+							</Pressable>
+						)}
+						{!isNotesCategory && item.timeSpent && (
+							<Text style={styles.timeText}>{formatTaskTime(item.timeSpent)}</Text>
+						)}
+						<Pressable 
+							onPress={() => handleDeleteTask(item.id)}
+							style={styles.actionButton}
+						>
+						<View style={styles.playButton}>
+							<Feather name="trash" size={20} color="white" />
+						</View>
+						</Pressable>
+						</View>
+					</View>		
+					{isNotesCategory && item.notes && (
+						<Text style={styles.taskNotes}>{item.notes}</Text>
+					)}
+				
+					<Text style={styles.createdAt}>
+						{new Date(item.createdAt).toLocaleDateString()}
+					</Text>
+				</Card.Content>
+          	</Card>
+			)}
+			renderSectionHeader={({ section }) => (
+          	<View style={styles.sectionHeader}>
+				<Text style={styles.sectionTitle}>{section.title}</Text>
+				{!isNotesCategory && (
+					<Text style={styles.sectionTime}>{section.totalTime}</Text>
+				)}
+          	</View>
+        	)}
+			contentContainerStyle={styles.listContent}
+			ListEmptyComponent={
+          		<Text style={styles.emptyText}>Nenhuma tarefa criada ainda</Text>
+        	}/>
+			<Pressable style={styles.addButton} onPress={() => setModalVisible(true)}>
+        		<Feather name="plus" size={24} color="white" />
+      		</Pressable>
       
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>Nova Tarefa</Text>
-            
-            <TextInput
-              style={styles.input}
-              placeholder="Título da tarefa"
-              value={newTaskTitle}
-              onChangeText={setNewTaskTitle}
-            />
-            
-            {isNotesCategory && (
-              <TextInput
-                style={[styles.input, styles.notesInput]}
-                placeholder="Anotações"
-                multiline
-                numberOfLines={4}
-                value={newTaskNotes}
-                onChangeText={setNewTaskNotes}
-              />
-            )}
-            
-            <View style={styles.modalButtons}>
-              <Pressable
-                style={[styles.modalButton, styles.cancelButton]}
-                onPress={() => setModalVisible(false)}
-              >
-                <Text style={styles.buttonText}>Cancelar</Text>
-              </Pressable>
-              
-              <Pressable
-                style={[styles.modalButton, styles.addButtonModal]}
-                onPress={handleAddTask}
-              >
-                <Text style={styles.buttonText}>Adicionar</Text>
-              </Pressable>
-            </View>
-          </View>
-        </View>
-      </Modal>
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => setModalVisible(false)}>
+        		<View style={styles.modalContainer}>
+          			<View style={styles.modalContent}>
+						<Text style={styles.modalTitle}>Nova Tarefa</Text>
+						<TextInput
+							style={styles.input}
+							placeholder="Título da tarefa"
+							value={newTaskTitle}
+							onChangeText={setNewTaskTitle} />
+							{isNotesCategory && (
+						<TextInput
+							style={[styles.input, styles.notesInput]}
+							placeholder="Anotações"
+							multiline
+							numberOfLines={4}
+							value={newTaskNotes}
+							onChangeText={setNewTaskNotes} />)}
+						<View style={styles.modalButtons}>
+							<Pressable
+								style={[styles.modalButton, styles.cancelButton]}
+								onPress={() => setModalVisible(false)}>
+								<Text style={styles.buttonText}>Cancelar</Text>
+							</Pressable>
+							<Pressable
+								style={[styles.modalButton, styles.addButtonModal]}
+								onPress={handleAddTask}>
+								<Text style={styles.buttonText}>Adicionar</Text>
+							</Pressable>
+            			</View>
+          			</View>
+        		</View>
+      		</Modal>
     </View>
   );
 }
@@ -309,62 +315,108 @@ const handleDeleteTask = async (taskId: string) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#F8F2EF",
   },
-  title: {
-    fontSize: 20,
+  titleContainer: {
+    marginVertical: 60,
+    marginBottom: 40,
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginLeft: 15,
+  },
+  titleIconContainer: {
+	  flexDirection: "row",
+	  justifyContent: "space-between",
+	  alignItems: "center",
+	  width: "94%",
+	},
+	iconView: {
+		justifyContent: "center",
+    alignItems: "center",
+    width: 50,
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: "white",
+},
+title: {
+	fontSize: 22,
     fontWeight: 'bold',
     marginBottom: 16,
-    color: '#333',
-  },
-  taskCard: {
-    marginBottom: 12,
+    color: '#6B1E9C',
+},
+taskCard: {
     backgroundColor: 'white',
-    borderRadius: 8,
-    elevation: 2,
-  },
-  taskHeader: {
-    flexDirection: 'row',
+    borderRadius: 15,
+	borderWidth: 1.5,
+	borderColor: "white",
+    elevation: 4,
+	margin: 15,
+	marginBottom: 5,
+},
+taskHeader: {
+	flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 8,
-  },
-  taskTitle: {
-    fontSize: 16,
+},
+taskTitle: {
+	fontSize: 19,
     fontWeight: '600',
     flex: 1,
-  },
-  playButton: {
-    flexDirection: 'row',
+},
+playButton: {
+	backgroundColor: "#6B1E9C",
+	flexDirection: 'row',
     alignItems: 'center',
-    padding: 8,
-  },
-  timeText: {
-    marginLeft: 8,
-    color: '#666',
+    padding: 7,
+	borderRadius: 50,
+	borderWidth: 0.8,
+	borderColor: "#6B1E9C",
+	elevation: 2,
+},
+timeText: {
+	margin: 0,
+    color: '#333',
     fontSize: 14,
-  },
-  taskNotes: {
-    color: '#666',
-    marginTop: 8,
-    fontSize: 14,
-  },
-  createdAt: {
-    color: '#999',
+},
+taskNotes: {
+	backgroundColor: 'white',
+	color: '#667',
+    marginTop: 0,
+    fontSize: 15,
+	padding: 10,
+	marginBottom: 10,
+	borderWidth: 0.9,
+	borderColor: "white",
+	borderRadius: 15,
+	elevation: 2,
+},
+createdAt: {
+	width: "35%",
+	color: '#6B1E9C',
     fontSize: 12,
-    marginTop: 8,
-  },
-  listContent: {
-    paddingBottom: 80,
-  },
-  emptyText: {
-    textAlign: 'center',
+	fontWeight: "600",
+    marginTop: 0,
+	padding: 6,
+	textAlign: "center",
+	borderWidth: 0.8,
+	borderColor: "white",
+	borderRadius: 20,
+	backgroundColor: "white",
+	elevation: 2,
+},
+listContent: {
+    paddingBottom: 50,
+},
+emptyText: {
+	textAlign: 'center',
     color: '#999',
     marginTop: 32,
-  },
-  addButton: {
-    position: 'absolute',
+},
+addButton: {
+	position: 'absolute',
     right: 24,
     bottom: 24,
     backgroundColor: '#6B1E9C',
@@ -374,77 +426,77 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     elevation: 4,
-  },
-  modalContainer: {
-    flex: 1,
+},
+modalContainer: {
+	flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.5)',
-  },
-  modalContent: {
-    width: '80%',
+},
+modalContent: {
+	width: '80%',
     backgroundColor: 'white',
     borderRadius: 8,
     padding: 16,
-  },
-  modalTitle: {
-    fontSize: 18,
+},
+modalTitle: {
+	fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 16,
-  },
-  input: {
-    borderWidth: 1,
+},
+input: {
+	borderWidth: 1,
     borderColor: '#ddd',
     borderRadius: 4,
     padding: 12,
     marginBottom: 16,
-  },
-  notesInput: {
-    height: 100,
+},
+notesInput: {
+	height: 100,
     textAlignVertical: 'top',
-  },
-  modalButtons: {
-    flexDirection: 'row',
+},
+modalButtons: {
+	flexDirection: 'row',
     justifyContent: 'flex-end',
-  },
-  modalButton: {
-    paddingVertical: 8,
+},
+modalButton: {
+	paddingVertical: 8,
     paddingHorizontal: 16,
     borderRadius: 4,
     marginLeft: 8,
-  },
-  cancelButton: {
-    backgroundColor: '#ddd',
-  },
-  addButtonModal: {
-    backgroundColor: '#6B1E9C',
-  },
-  buttonText: {
-    color: 'white',
-  },
-  
-  sectionHeader: {
+},
+cancelButton: {
+	backgroundColor: '#ddd',
+},
+addButtonModal: {
+	backgroundColor: '#6B1E9C',
+},
+buttonText: {
+	color: 'white',
+},
+
+sectionHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#e0e0e0',
-    padding: 10,
-    marginTop: 10,
-    borderRadius: 5,
+    marginLeft: 15,
+	paddingBottom: 15,
   },
   sectionTitle: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#333',
   },
   sectionTime: {
-    fontSize: 14,
-    color: '#666',
+    fontSize: 16,
+	paddingRight: 15,
+    color: '#333',
     fontWeight: '600',
   },
 taskActions: {
   flexDirection: 'row',
   alignItems: 'center',
+//   backgroundColor: "red"
 },
 actionButton: {
   marginLeft: 12,
